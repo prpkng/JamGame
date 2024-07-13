48d72755eaeeed887abc7f1f01b95c0ab75f3ea0
Purpkin <guiejo2020@gmail.com>
2024-07-12 21:29:10 -0300

    Initial Commit

---
+++ Assets/Scripts/Player/ThirdPersonMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    [SerializeField] private CharacterController character;
+    [SerializeField] private float speed;
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
