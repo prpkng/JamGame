fd71a18a24c7077fe817e029d3cee03d84595e7b
Purpkin <guiejo2020@gmail.com>
2024-08-16 23:06:39 -0300

    BGM integration

---
+++ Assets/Scripts/Systems/Sound/BGMManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Game.Systems.Sound
+{
+    public class BGMManager : MonoBehaviour
+    {
+
+        private static FMOD.Studio.EventInstance BGMEventInstance;
+
+        [SerializeField] private FMODUnity.EventReference bgmEvent;
+
+
+        void Awake()
+        {
+            if (BGMEventInstance.isValid())
+                BGMEventInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            BGMEventInstance = FMODUnity.RuntimeManager.CreateInstance(bgmEvent);
+            BGMEventInstance.start();
+        }
+    }
+}
